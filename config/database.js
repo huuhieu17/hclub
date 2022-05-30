ad14d2b91ff722c584f104a897501bd2193120e0
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2022-05-31 01:34:19 +0700

    fix db

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@
 module.exports = {
 	'url': 'mongodb://un6aznrkkfh8me5rdmft:zkgXXYxSK0wKl8UqsrBn@bnxjz7875wlpfvn-mongodb.services.clever-cloud.com:27017/bnxjz7875wlpfvn',
 	'options': {
-		//'user':   'root',
-		//'pass':   '',
-		'dbName': 'server',
+		'user':   'un6aznrkkfh8me5rdmft',
+		'pass':   'zkgXXYxSK0wKl8UqsrBn',
+		'dbName': 'bnxjz7875wlpfvn',
 		'useNewUrlParser': true,
 		'useUnifiedTopology': true,
-		//'autoIndex':       false,
+		'autoIndex':       false,
 	},
 };
