ddfa9767e64df493a33d994ec7f7f7d427dcaf0c
Nguyen Huu Hieu <huuhieu1711@gmail.com>
2022-05-31 01:20:08 +0700

    add db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-	'url': 'mongodb://localhost:27017',
+	'url': 'mongodb://un6aznrkkfh8me5rdmft:zkgXXYxSK0wKl8UqsrBn@bnxjz7875wlpfvn-mongodb.services.clever-cloud.com:27017/bnxjz7875wlpfvn',
 	'options': {
 		//'user':   'root',
 		//'pass':   '',
